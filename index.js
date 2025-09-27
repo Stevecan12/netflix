bb32dbde81631e74753c892ad918f2f3ee2b21ca
Stevecan12 <aligadarin8@gmail.com>
2025-09-27 13:02:28 +0100

    jjjjj

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Our Javascript functions will go here later
+
+
+const questions = document.querySelectorAll('.faq-boxes .question')
+
+questions.forEach(question => {
+    question.addEventListener('click', ()=> {
+        // Find next sibling answer element
+        const answer = question.nextElementSibling;
+
+
+        // Toggle Display
+        if (answer.style.display === "none"){
+            answer.style.display = "block"
+        } else {
+            answer.style.display = "none"
+        }
+    })
+})  
